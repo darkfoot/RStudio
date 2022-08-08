a0ddca074c24ffb3d736585c2ef50f3143866baa
Kilian Verhelst <kilian.verhelst@gmx.de>
2022-08-08 10:04:59 +0200

    further cleanup added smooth line to lineplot

--- en_am_lineplot.R
+++ en_am_lineplot.R
@@ -1,19 +1,36 @@
 library(plotly)
 library(dplyr)
 
+# group data by no of words in text
 mean_freq <- frequency_data %>%
                 group_by(No..words.in.text) %>%
                 summarize(mean_freq = mean(Freq..per.hundred),
                           language = language)
 
-print(mean_freq)
+# set x, y data for later plots
+xb <- mean_freq$No..words.in.text[mean_freq$language == "B"]
+yb <- mean_freq$mean_freq[mean_freq$language == "B"]
+xa <- mean_freq$No..words.in.text[mean_freq$language == "A"]
+ya <- mean_freq$mean_freq[mean_freq$language == "A"]
 
-plot_ly(x = mean_freq$No..words.in.text[mean_freq$language == "B"],
-        y = mean_freq$mean_freq[mean_freq$language == "B"],
+# get new data by smoothing with loess
+smooth_b <- loess(yb ~ xb)
+smooth_a <- loess(ya ~ xa)
+
+# plot data
+plot_ly(x = xb,
+        y = yb,
         type = "scatter",
         mode = "lines",
         name = "Bri*ish") %>%
-  add_trace(x = mean_freq$No..words.in.text[mean_freq$language == "A"],
-            y = mean_freq$mean_freq[mean_freq$language == "A"],
-            name = "M'uerica")
+  add_trace(x = xa,
+            y = ya,
+            name = "M'uerica") %>%
+  add_trace(x = xb,
+            y = predict(smooth_b),
+            name = "Bri*ish smooth") %>%
+  add_trace(x = xa,
+            y = predict(smooth_a),
+            name = "Bri*ish smooth") %>%
+  layout(title = "pornwords")
 
